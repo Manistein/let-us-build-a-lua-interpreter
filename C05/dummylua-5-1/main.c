b55e50112eb09fee33bb1d1b3490482b8de8827d
manistein <1518986465@qq.com>
2022-01-18 20:48:48 +0800

    使用p9测试用例

--- C05/dummylua-5-1/main.c
+++ C05/dummylua-5-1/main.c
@@ -1,10 +1,11 @@
-#include "test/p8_test.h"
+#include "test/p9_test.h"
 #ifdef _WINDOWS_PLATFORM_
 #include <process.h>
 #endif
 
 int main(int argc, char** argv) {
-	p8_test_main();
+
+	p9_test_main();
 
 #ifdef _WINDOWS_PLATFORM_
 	system("pause");
