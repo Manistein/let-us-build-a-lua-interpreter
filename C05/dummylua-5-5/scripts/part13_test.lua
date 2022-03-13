4c0ba663d4eba9e3cebb093f3d4f13bf3830b83a
manistein <1518986465@qq.com>
2022-03-13 20:38:39 +0800

    windows上读取dll

--- C05/dummylua-5-5/scripts/part13_test.lua
+++ C05/dummylua-5-5/scripts/part13_test.lua
@@ -34,7 +34,7 @@ local mod2 = require("scripts/p13_test_return_table")
 mod2.hello()
 mod2.world()
 
-package.cpath = package.cpath .. "../test/?.so;"
+package.cpath = package.cpath .. "../test/?.dll;"
 print(package.cpath)
 local clib = require("loadlib")
 clib.hello_world()
